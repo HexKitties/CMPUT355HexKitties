d0aa0fb2f9ef11e6a51981465ba07aab218c638e
Jeremy0818 <sheeyeowchoo@gmail.com>
2020-11-17 15:36:12 -0700

    added check winner function, fixed bug from branch jiaxin

--- model.py
+++ model.py
@@ -1,7 +1,12 @@
 import globvar
+import copy
+from collections import deque
+
+
 
 class HexModel():
-    def __init__(self, radius=40, size=(8, 8), color=(128,128,128), players_color = ((255, 0, 0), (0, 0, 255)), mode = 0):
+    def __init__(self, radius=40, size=(4, 4), color=(128, 128, 128), players_color=((255, 0, 0), (0, 0, 255)), mode=0):
+
         self.radius = radius
         self.size = size
         self.default_color = color
@@ -12,22 +17,80 @@ class HexModel():
         self.modes = {0: "REAL PLAYER", 1: "AI PLAYER"}
         self.current_mode = mode
 
+        self.BTM_ROW = set()
+        for x in range(self.size[1]):
+            self.BTM_ROW.add((self.size[1] - 1, x) )
+        self.TOP_ROW = set()
+        for x in range(self.size[1]):
+            self.TOP_ROW.add((0, x))
+        self.LFT_COL = set()
+        for x in range(self.size[0]):
+            self.LFT_COL.add((x, 0))
+        self.RGT_COL = set()
+        for x in range(self.size[0]):
+            self.RGT_COL.add((x, self.size[0] - 1))
+
+        self.nbrs = {}
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                temp = []
+                if i > 0:
+                    temp.append((i - 1, j))
+                if i > 0 and j < self.size[1] - 1:
+                    temp.append((i - 1, j + 1))
+                if j > 0:
+                    temp.append((i, j - 1))
+                if j < self.size[1] - 1:
+                    temp.append((i, j + 1))
+                if i < self.size[0] - 1 and j > 0:
+                    temp.append((i + 1, j - 1))
+                if i < self.size[0] - 1:
+                    temp.append((i + 1, j))
+                self.nbrs[(i, j)] = temp
+        # print(self.BTM_ROW)
+        # print(self.TOP_ROW)
+        # print(self.LFT_COL)
+        # print(self.RGT_COL)
+        # print(self.nbrs)
+
     def init_board(self):
-    	empty_brd = []
-    	for i in range(self.size[0]):
-    		temp = []
-    		for i in range(self.size[1]):
-    			temp.append(-1)
-    		empty_brd.append(temp)
-    	return empty_brd
+        empty_brd = []
+        for i in range(self.size[0]):
+            temp = []
+            for i in range(self.size[1]):
+                temp.append(-1)
+            empty_brd.append(temp)
+        return empty_brd
+
+    def get_current_mode(self):
+        return self.current_mode
+
+    def move(self):
+        monte = monte_carlo.MonteCarlo(self)
+        next_move = monte.get_move(120)
+        self.place_chess(next_move)
 
     def place_chess(self, chess_pos):
         if self.board[chess_pos[0]][chess_pos[1]] == -1:
             self.board[chess_pos[0]][chess_pos[1]] = self.player_turn
             self.player_turn = (self.player_turn + 1) % 2
-            self.history.append(current_mouse)
+            self.history.append(chess_pos)
+            check = self.get_winner()
+            print(check)
+            if check != 2:
+                self.board = self.init_board()
             return True
         return False
+
+    def place_chess2(self, chess_pos):
+        board = [self]
+        board[0].board[chess_pos[0]][chess_pos[1]] = board[0].player_turn
+        board[0].player_turn = (board[0].player_turn + 1) % 2
+        board[0].history.append(chess_pos)
+        return board[0]
+
+    def str_rep(self):
+        return ''.join([str(elem) for elem in self.board])
 
     def new_game(self):
         self.board = self.init_board()
@@ -36,12 +99,57 @@ class HexModel():
         self.current_mode = (self.current_mode + 1) % 2
 
     def undo(self):
-    	if len(self.history) != 0:
-    		last_pos = self.history.pop()
-    		self.board[last_pos[0]][last_pos[1]] = -1
-    		self.player_turn = (self.player_turn + 1) % 2
-    		return True
-    	return False
+        if len(self.history) != 0:
+            last_pos = self.history.pop()
+            self.board[last_pos[0]][last_pos[1]] = -1
+            self.player_turn = (self.player_turn + 1) % 2
+            return True
+        return False
 
     def notify_update(self, mouse_pos):
         globvar.hex_view.display(self.radius, self.size, self.default_color, self.board, mouse_pos)
+
+    def legal_moves(self):
+        temp = []
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                if self.board[i][j] == -1:
+                    temp.append((i, j))
+        return temp
+
+    def current_player(self):
+        return self.player_turn
+
+    def get_winner(self):
+        set1, set2 = (self.TOP_ROW, self.BTM_ROW)
+        # print('has_win', brd, who, set1, set2)
+        Q, seen = deque([]), set()
+        for c in set1:
+            if self.board[c[0]][c[1]] == 0:
+                Q.append(c)
+                seen.add(c)
+        while len(Q) > 0:
+            c = Q.popleft()
+            if c in set2:
+                return 0
+            for d in self.nbrs[c]:
+                if self.board[d[0]][d[1]] == 0 and d not in seen:
+                    Q.append(d)
+                    seen.add(d)
+
+        set1, set2 = (self.LFT_COL, self.RGT_COL)
+        # print('has_win', brd, who, set1, set2)
+        Q, seen = deque([]), set()
+        for c in set1:
+            if self.board[c[0]][c[1]] == 1:
+                Q.append(c)
+                seen.add(c)
+        while len(Q) > 0:
+            c = Q.popleft()
+            if c in set2:
+                return 1
+            for d in self.nbrs[c]:
+                if self.board[d[0]][d[1]] == 1 and d not in seen:
+                    Q.append(d)
+                    seen.add(d)
+        return 2
