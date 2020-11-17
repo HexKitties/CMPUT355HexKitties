a38689005dcadef1ee435ac40af41a0c6f5665da
youwei1-sudo <davidchenyouwei1999@gmail.com>
2020-11-17 13:44:38 -0800

    haha

--- model.py
+++ model.py
@@ -12,6 +12,12 @@ class HexModel():
         self.modes = {0: "REAL PLAYER", 1: "AI PLAYER"}
         self.current_mode = mode
 
+        
+        self.BTM_ROW = ((self.size[1] - 1, x) for x in range(self.size[1]))
+        self.TOP_ROW = ((0, x) for x in range(self.size[1]))
+        self.LFT_COL = ((x, 0) for x in range(self.size[0]))
+        self.RGT_COL = ((x, self.size[0] - 1) for x in range(self.size[0]))
+
     def init_board(self):
     	empty_brd = []
     	for i in range(self.size[0] ):
@@ -48,3 +54,38 @@ class HexModel():
 
     def notify_wrongly(self,text):
         globvar.hex_view.draw_wrong_notify(text)
+
+    # Youwei continue working from JiaXin
+    def get_winner(self):
+        set1, set2 = (self.TOP_ROW, self.BTM_ROW)
+        # print('has_win', brd, who, set1, set2)
+        Q, seen = deque([]), set()
+        for c in set1:
+            if self.board[c[0]][c[1]] == 0:
+                Q.append(c)
+                seen.add(c)
+        while len(Q) > 0:
+            c = Q.popleft()
+            if c in set2:
+                return 0
+            for d in self.nbrs[c]:
+                if self.board[d[0]][d[1]] == 0 and d not in seen:
+                    Q.append(d)
+                    seen.add(d)
+
+        set1, set2 = (self.LFT_COL, self.RGT_COL)
+        # print('has_win', brd, who, set1, set2)
+        Q, seen = deque([]), set()
+        for c in set1:
+            if self.board[c[0]][c[1]] == 0:
+                Q.append(c)
+                seen.add(c)
+        while len(Q) > 0:
+            c = Q.popleft()
+            if c in set2:
+                return 1
+            for d in self.nbrs[c]:
+                if self.board[d[0]][d[1]] == 0 and d not in seen:
+                    Q.append(d)
+                    seen.add(d)
+        return 2
