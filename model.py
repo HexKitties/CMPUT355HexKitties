dc7621d14d882c3a2028308d35f632b37981a976
Jeremy0818 <sheeyeowchoo@gmail.com>
2020-11-16 18:44:56 -0700

    changed variable name in model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class HexModel():
         self.default_color = color
         self.players = players_color
         self.player_turn = 0
-        self.chess_pos = self.init_board()
+        self.board = self.init_board()
         self.history = []
         self.modes = {0: "REAL PLAYER", 1: "AI PLAYER"}
         self.current_mode = mode
@@ -21,16 +21,16 @@ class HexModel():
     		empty_brd.append(temp)
     	return empty_brd
 
-    def place_chess(self, current_mouse):
-        if self.chess_pos[current_mouse[0]][current_mouse[1]] == -1:
-            self.chess_pos[current_mouse[0]][current_mouse[1]] = self.player_turn
+    def place_chess(self, chess_pos):
+        if self.board[chess_pos[0]][chess_pos[1]] == -1:
+            self.board[chess_pos[0]][chess_pos[1]] = self.player_turn
             self.player_turn = (self.player_turn + 1) % 2
             self.history.append(current_mouse)
             return True
         return False
 
     def new_game(self):
-        self.chess_pos = self.init_board()
+        self.board = self.init_board()
 
     def switch_mode(self):
         self.current_mode = (self.current_mode + 1) % 2
@@ -38,10 +38,10 @@ class HexModel():
     def undo(self):
     	if len(self.history) != 0:
     		last_pos = self.history.pop()
-    		self.chess_pos[last_pos[0]][last_pos[1]] = -1
+    		self.board[last_pos[0]][last_pos[1]] = -1
     		self.player_turn = (self.player_turn + 1) % 2
     		return True
     	return False
 
     def notify_update(self, mouse_pos):
-        globvar.hex_view.display(self.radius, self.size, self.default_color, self.chess_pos, mouse_pos)
+        globvar.hex_view.display(self.radius, self.size, self.default_color, self.board, mouse_pos)
