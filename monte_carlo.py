f5ada76b6ccb2b15b0f474c3a5f24656953110f5
Jiaxin Wang <jiaxin13@ualberta.ca>
2020-11-17 19:00:06 -0700

    mcts

---
+++ monte_carlo.py
@@ -0,0 +1,93 @@
+import time
+from math import log, sqrt
+
+import model
+import random
+import copy
+
+
+class MonteCarlo():
+    def __init__(self, board):
+        self.board: model.HexModel = board
+        self.states = []
+        self.plays = {}
+        self.wins = {}
+        self.parameter = 2
+
+    def get_move(self, max_time):
+        player = self.board.current_player()
+        print('player:', player)
+        blanks = self.board.legal_moves(self.board.board)
+        start = time.time()
+        while time.time() - start < max_time:
+            self.run_simulation()
+
+        next_states = []
+        for x in blanks:
+            state_temp = copy.deepcopy(self.board.board)
+            next_states.append((x, self.board.place_chess2(x, state_temp)))
+
+
+        precent_wins, move = max(
+            (self.wins.get((player, self.list_to_str(s)), 0) /
+             self.plays.get((player, self.list_to_str(s)), 1),
+             p)
+            for p, s in next_states
+        )
+        print(precent_wins)
+        return precent_wins, move
+
+    def run_simulation(self):
+        plays = self.plays
+        wins = self.wins
+        state = copy.deepcopy(self.board.board)
+        visited = []
+        player = self.board.current_player()
+        # player = self.board.last_player(state)
+
+        expand = True
+        while True:
+            blanks = self.board.legal_moves(state)
+
+            next_states = []
+            for x in blanks:
+                state_temp = copy.deepcopy(state)
+                next_states.append((x, self.board.place_chess2(x, state_temp)))
+
+            # print(next_states[0])
+
+            if all(plays.get((player, self.list_to_str(s))) for _, s in next_states):
+                log_num_simulation = log(sum(plays[(player, self.list_to_str(s))] for _, s in next_states))
+                value, move, state = max(
+                    ((wins[(player, self.list_to_str(s))] / plays[(player, self.list_to_str(s))]) +
+                     self.parameter * sqrt(log_num_simulation / plays[(player, self.list_to_str(s))]), p, s)
+                    for p, s in next_states
+                )
+                # print('value:', value, 'move:', move, 'state:', state)
+            else:
+                move, state = random.choice(next_states)
+
+            if expand and (player, self.list_to_str(state)) not in self.plays:
+                expand = False
+                self.plays[(player, self.list_to_str(state))] = 0
+                self.wins[player, self.list_to_str(state)] = 0
+
+            visited.append((player, self.list_to_str(state)))
+            player = self.board.last_player(state)
+            winner = self.board.get_winner(state)
+            if winner != 2:
+                break
+        for player, board in visited:
+
+            if (player, board) not in self.plays:
+                continue
+            self.plays[(player, board)] += 1
+            if player == winner:
+                self.wins[(player, board)] += 1
+
+    def list_to_str(self, l):
+        temp = ''
+        for i in range(len(l)):
+            for j in range(len(l[0])):
+                temp += str(l[i][j])
+        return temp
--- view.py
+++ view.py
@@ -99,10 +99,14 @@ class HexView():
     def check_mouse(self, radius, mouse_pos, center_pos):
     	return (mouse_pos[0]-center_pos[0])**2 + (mouse_pos[1] - center_pos[1])**2 < (radius * 0.8)**2
 
-    def display(self, radius, size, default_color, chess_pos, mouse_pos):
+    def display(self, radius, size, default_color, chess_pos, mouse_pos, text):
     	# Fill the background with white
         self.screen.fill(self.bgr_color)
         self.draw_buttons(mouse_pos)
+        #print(globvar.hex_ctrl.print_message)
+        if globvar.hex_ctrl.print_message:
+            print("here", text)
+            self.draw_wrong_notify(text)
         pos = self.start_pos
         in_brd = False
 
@@ -137,6 +141,28 @@ class HexView():
         if not in_brd:
         	globvar.hex_ctrl.pos_on_board = None
 
+    def draw_wrong_notify(self,text):
+        # pygame.font.init() # you have to call this at the start,
+        #            # if you want to use this module.
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+        # text = myfont.render('Do not place chess out of board ', False, (0, 0, 0))
+        # text = 'Do not place chess out of board '
+        # textRect = text.get_rect()
+        # textRect.center = self.screen.center
+        # self.screen.blit(text, textRect)
+        # self.screen.blit(text)
+
+        largeText = pygame.font.Font('freesansbold.ttf',50)
+        TextSurf, TextRect = self.text_objects(text, largeText)
+        TextRect.center = ((self.size[0]/2),(self.size[1]/2))
+        self.screen.blit(TextSurf, TextRect)
+        pygame.display.update()
+        pass
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, (0,0,0))
+        return textSurface, textSurface.get_rect()
+
     # def get_pos(self, radius, size, pos, mouse_pos):
     #     for i in range(size[0]):
     #         for j in range(size[1]):
@@ -147,4 +173,3 @@ class HexView():
     #         d_y = cos((60 / 180) * 3.14) * radius + radius
     #         pos = (int(pos[0] + d_y),\
     #          int(pos[1] + d_x))
-
